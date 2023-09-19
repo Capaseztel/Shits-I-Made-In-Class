dfa0932120a2a14913fb7d0cc4204df1d847f4b2
Capaseztel <106227790+Capaseztel@users.noreply.github.com>
2023-09-19 09:34:55 +0200

    change test

--- payloads/test.py
+++ payloads/test.py
@@ -1,3 +1,7 @@
 import os
+import getpass
 
-os.system("sudo apt install python3-pip")
+user = getpass.getuser()
+print("user = " + user)
+os.system("cd Imágenes && wget --output-document=pikmin.jpg https://i.ytimg.com/vi/ppYx1kaji5w/maxresdefault.jpg")
+os.system(f"gsettings get org.gnome.desktop.background picture-uri file:///home/{user}/Imágenes/pikmin.jpg")

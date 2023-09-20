f2b5b271dca16207ce728cf738e7d0b1a4381ca5
Capaseztel <106227790+Capaseztel@users.noreply.github.com>
2023-09-20 09:29:55 +0200

    fix

--- payloads/test.py
+++ payloads/test.py
@@ -4,4 +4,4 @@ import getpass
 user = getpass.getuser()
 print("user = " + user)
 os.system("cd Imágenes && wget --output-document=pikmin.jpg https://i.ytimg.com/vi/ppYx1kaji5w/maxresdefault.jpg")
-os.system(f"gsettings get org.gnome.desktop.background picture-uri file:///home/{user}/Imágenes/pikmin.jpg")
+os.system(f"gsettings set org.gnome.desktop.background picture-uri file:///home/{user}/Imágenes/pikmin.jpg")

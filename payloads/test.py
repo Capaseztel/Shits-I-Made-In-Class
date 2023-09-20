548d724365ac0cfcf66c70c7c218944bb7fe5b8f
Capaseztel <106227790+Capaseztel@users.noreply.github.com>
2023-09-20 09:32:29 +0200

    change image

--- payloads/test.py
+++ payloads/test.py
@@ -3,5 +3,5 @@ import getpass
 
 user = getpass.getuser()
 print("user = " + user)
-os.system("cd Imágenes && wget --output-document=pikmin.jpg https://i.ytimg.com/vi/ppYx1kaji5w/maxresdefault.jpg")
+os.system("cd Imágenes && wget --output-document=pikmin.jpg https://pbs.twimg.com/media/EYzIYbcXgAAf3Gp.jpg")
 os.system(f"gsettings set org.gnome.desktop.background picture-uri file:///home/{user}/Imágenes/pikmin.jpg")
